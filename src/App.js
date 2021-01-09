7b9f35ca6bf4d72e140d6c9c2efec21bef289af0
rachelseaborn <raseaborn@gmail.com>
2021-01-09 16:22:45 -0700

    interim - responsiveness works but ugghh on the rest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobileMenuView: false
+    }
+  }
+
+  toggleMenu = () => {
+    this.setState({ mobileMenuView: !this.state.mobileMenuView })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+
+          {/* <div className='laptop-nav'> */}
+          <nav className='laptop-menu'>
+            <span>About</span>
+            <span>Projects</span>
+            <span>Contact</span>
+          </nav>
+          {/* </div> */}
+
+          <div className='mobile-nav' onClick={this.toggleMenu}>Menu</div>
+          {this.state.mobileMenuView
+            ? (
+              <nav className='mobile-menu'>
+                <span>About</span>
+                <span>Projects</span>
+                <span>Contact</span>
+              </nav>
+            )
+            : null}
+
+
+          <section className="landing-text">
+
+            <h1>GRAYSCALE</h1>
+            <p>A free, responsive, one page Bootstrap theme</p>
+            <p>created by Start Bootstrap.</p>
+
+          </section>
+
+          <button onClick={console.log('learn more')}>GET STARTED</button>
+
+        </header >
+      </div >
+
+    );
+  }
 }
 
 export default App;
